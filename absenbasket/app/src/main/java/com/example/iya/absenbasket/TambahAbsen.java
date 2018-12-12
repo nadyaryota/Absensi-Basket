7e535a70bd2f9d74ee0d40f37ba578e68bc45412
komputer-PC\komputer <pratiwi>
2018-12-12 18:15:14 +0700

    menambahkan menu dan tambah absen

---
+++ absenbasket/app/src/main/java/com/example/iya/absenbasket/TambahAbsen.java
@@ -0,0 +1,25 @@
+package com.example.iya.absenbasket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class TambahAbsen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_absen);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner);
+//create a list of items for the spinner.
+        String[] items = new String[]{"Hadir", "Tidak Hadir"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+    }
+}
